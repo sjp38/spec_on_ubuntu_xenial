37b341ec353260e912c9a828430d468a1697aff7
SeongJae Park <sj38.park@gmail.com>
2017-09-24 12:32:55 +0900

    Add patch for Ubuntu Xenial
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ patch-for-ubuntu16.04.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BINDIR=`dirname $0`
+pushd $BINDIR
+
+SRCDIR=./spec_src
+
+if [ ! -d $SRCDIR ]
+then
+	echo "[Error] spec source code not found under $SRCDIR"
+	exit 1
+fi
+
+pushd $SRCDIR
+patch -p1 < ../for-ubuntu16.04.patch
+popd
+
+popd
